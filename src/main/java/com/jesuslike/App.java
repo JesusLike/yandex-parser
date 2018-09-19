45058151175b381a159f25e22bf38f43003ad510
Yudovich <yudovichml@gmail.com>
2018-09-19 16:41:52 +0300

    working version

--- src/main/java/com/jesuslike/App.java
+++ src/main/java/com/jesuslike/App.java
@@ -14,8 +14,8 @@ public class App {
     }
 
     void run() throws IOException {
-        String url = "http://yandex.ru/yandsearch?text=%D0%9C%D0%B0%D0%B9%D1%81%D0%BA" +
-                "%D0%B0%D1%8F+%D1%82%D1%80%D0%B0%D0%B2%D0%BA%D0%B0+" +
+        String url = "http://yandex.ru/yandsearch?text=%D0%9C%D0%B0%D0%B9%D1%81%D0%BA" +    //Can be easily reworked
+                "%D0%B0%D1%8F+%D1%82%D1%80%D0%B0%D0%B2%D0%BA%D0%B0+" +                      //to get url from user input
                 "%D0%B3%D0%BE%D0%BB%D0%BE%D0%B4%D0%BD%D0%BE%D0%B3%D0%BE+" +
                 "%D0%BA%D0%BE%D1%80%D0%BC%D0%B8%D1%82&nl=1&lr=2";
         Document doc = Jsoup.connect(url).get();
